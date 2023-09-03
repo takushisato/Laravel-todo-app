ee21aaf044c6a7f62f08b8ad80129731acb3de81
takushisato <takuchanpower@gmail.com>
2023-09-03 12:35:50 +0900

    テストデータ作成

--- database/factories/ToDoFactory.php
+++ database/factories/ToDoFactory.php
@@ -17,7 +17,7 @@ class ToDoFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->name()
         ];
     }
 }
